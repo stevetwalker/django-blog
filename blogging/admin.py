45f8bca8bdc29acb9efbdaafd381a910d30c1882
stevetwalker <29964517+stevetwalker@users.noreply.github.com>
2020-05-17 14:48:45 -0700

    Added blog categories

--- blogging/admin.py
+++ blogging/admin.py
@@ -3,5 +3,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from blogging.models import Post
+from blogging.models import Post, Category
 admin.site.register(Post)
+admin.site.register(Category)
