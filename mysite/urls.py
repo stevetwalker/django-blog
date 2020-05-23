f4c03c14cd2835540c79ad1d50ad7a9fac7fefc4
stevetwalker <29964517+stevetwalker@users.noreply.github.com>
2020-05-23 13:46:26 -0700

    replaced path with url in urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path, include
+from django.urls import url, include
 
 urlpatterns = [
-    path('', include('blogging.urls')),
-    path('polling/', include('polling.urls')),
-    path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+    url('', include('blogging.urls')),
+    url('polling/', include('polling.urls')),
+    url('admin/', admin.site.urls),
+    url('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    url('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
